cd96c135aa2fe299dcb3fc96e6af2bc2532f0b75
SANTIAGO\sanga <s2808621@gmail.com>
2023-03-13 18:25:10 -0500

    urls

--- gestorCapiro-main/src/environments/environment.prod.ts
+++ gestorCapiro-main/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment = {
 //export const URL_API= `https://arrendamientoscapiro.com/`;
 
 //pruebas desarrollo
-export const URL_API= `http://44.199.218.210:9099/pruebas/`;
+export const URL_API= `http://54.196.245.80:9099/pruebas/`;
 
 //pruebas local
 //export const URL_API= `http://localhost:9099/pruebas/`;
