0d0b4f037863d55b358e850c88a172d93e538788
Edgar Muñoz <edgmuo1085@gmail.com>
2023-03-13 22:04:04 -0500

    se agrega prettier

--- gestorCapiro-main/src/app/Services/login.service.ts
+++ gestorCapiro-main/src/app/Services/login.service.ts
@@ -1,65 +1,57 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { Observable, Subject } from "rxjs";
-import { URL_API } from "src/environments/environment";
-import { tap } from "rxjs/operators";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { URL_API } from 'src/environments/environment';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
-    providedIn: "root",
+  providedIn: 'root',
 })
-
 export class LoginService {
+  estadoUsuario!: string;
+  estado = new Subject<any>();
+  nombreUsuario = new Subject<any>();
+  idUsuario = new Subject<any>();
 
-    estadoUsuario!: string;
-    estado = new Subject<any>();
-    nombreUsuario = new Subject<any>();
-    idUsuario = new Subject<any>();
+  constructor(private http: HttpClient) {}
 
-    constructor(private http: HttpClient) {
+  getMessage(): Observable<any> {
+    return this.estado.asObservable();
+  }
 
-    }
+  login(correo: string, contra: string): Observable<any> {
+    let json = { usuario: correo, password: contra };
+    return this.http.post<any>(`${URL_API}login/loguear/`, json).pipe(
+      tap((resp: any) => {
+        if (resp !== null) {
+          if (resp.token === null) {
+            localStorage.removeItem('token');
+          } else {
+            localStorage.setItem('dataUser', JSON.stringify(resp));
+            localStorage.setItem('token', resp.token);
+            this.estado.subscribe(resp);
+            this.estado.next(resp.estado);
+            this.nombreUsuario.subscribe(resp);
+            this.nombreUsuario.next(resp.nombres);
+            this.idUsuario.subscribe(resp);
+            this.idUsuario.next(resp.id);
+          }
+        }
+      })
+    );
+  }
 
-    getMessage(): Observable<any> {
-        return this.estado.asObservable();
-    }
-
-    login(correo: string, contra: string): Observable<any> {
-        let json = {"usuario":correo,"password":contra}
-        return this.http.post<any>(`${URL_API}login/loguear/`, json).pipe(
-            tap((resp: any) => {
-                if (resp !== null) {
-                    if (resp.token === null) {
-                        localStorage.removeItem('token');
-                    } else {
-                        localStorage.setItem('dataUser',JSON.stringify(resp))
-                        localStorage.setItem('token', resp.token);
-                        this.estado.subscribe(resp);
-                        this.estado.next(resp.estado);
-                        this.nombreUsuario.subscribe(resp);
-                        this.nombreUsuario.next(resp.nombres);
-                        this.idUsuario.subscribe(resp);
-                        this.idUsuario.next(resp.id);
-                    }
-                } 
-
-            }),
-        );
-
-    }
-
-
-    registrarUsuario(data:any){
-        return this.http.post<any>(`${URL_API}usuario/crear/`,data).pipe(
-            tap((resp: any) => {
-                if (resp !== null) {
-                    if (resp.token === null) {
-                        localStorage.removeItem('token');
-                    } else {
-                        localStorage.setItem('token', resp.token);
-                    }
-                } 
-            }),
-        );
-    }
-
+  registrarUsuario(data: any) {
+    return this.http.post<any>(`${URL_API}user/crear/`, data).pipe(
+      tap((resp: any) => {
+        if (resp !== null) {
+          if (resp.token === null) {
+            localStorage.removeItem('token');
+          } else {
+            localStorage.setItem('token', resp.token);
+          }
+        }
+      })
+    );
+  }
 }
--- gestorCapiro-main/src/app/pages/registro/registro.component.ts
+++ gestorCapiro-main/src/app/pages/registro/registro.component.ts
@@ -2,36 +2,40 @@ import { LoginService } from './../../Services/login.service';
 import { Router } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.css']
+  styleUrls: ['./registro.component.css'],
 })
 export class RegistroComponent implements OnInit {
-
   formRegistro!: FormGroup;
 
-  constructor(private fb: FormBuilder, private router: Router,
-              private loginService: LoginService) { }
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private loginService: LoginService
+  ) {}
 
   ngOnInit(): void {
     this.formRegistro = this.fb.group({
       nombres: ['', Validators.required],
-      cedula: ['', Validators.required],
+      apellidos: ['', Validators.required],
       correo: ['', Validators.required],
-      contra: ['', Validators.required],
-      parametroEmpresa: ['900990990']
-    })
+      identificacion: ['', Validators.required],
+      tipoIdentificacion: ['CC', Validators.required],
+      password: ['', Validators.required],
+    });
   }
 
-  registrar(){
-    if(this.formRegistro.valid){
-      this.loginService.registrarUsuario(this.formRegistro.value).subscribe( resp => {
-        console.log(resp);
-        
-      })
+  registrar() {
+    if (this.formRegistro.valid) {
+      this.loginService
+        .registrarUsuario(this.formRegistro.value)
+        .subscribe((resp) => {
+          console.log(resp);
+        });
       Swal.fire({
         icon: 'success',
         title: 'Bienvenido',
@@ -41,14 +45,13 @@ export class RegistroComponent implements OnInit {
         if (result.isConfirmed) {
           this.router.navigateByUrl('home');
         }
-      })
+      });
       console.log(this.formRegistro.value);
-    } else{
+    } else {
       Swal.fire({
         icon: 'info',
         text: 'Para realizar el registro es necesario ingresar todos los campos.',
-      })
+      });
     }
   }
-
 }
